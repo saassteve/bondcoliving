a8cc67603404328fe3ccfb5ab925f598af769624
saassteve <166345757+saassteve@users.noreply.github.com>
2025-11-18 17:07:59 +0000

    Updated MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -168,8 +168,6 @@ const MainLayout: React.FC = () => {
           </div>
         </div>
 
-        {/* Sticky Booking Bar (Preserved functionality) */}
-        <StickyBookingBar />
 
         {/* Main Content */}
         <main className="flex-grow pt-0">
