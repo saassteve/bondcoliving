d5c00af781d57175027ca583396d403e942f3604
saassteve <166345757+saassteve@users.noreply.github.com>
2025-11-18 20:01:03 +0000

    Updated Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Home, Coffee, ArrowDown } from 'lucide-react';
-import AnimatedSection from './AnimatedSection';
+import AnimatedSection from '../AnimatedSection';
 
 const Hero: React.FC = () => {
   const navigate = useNavigate();
